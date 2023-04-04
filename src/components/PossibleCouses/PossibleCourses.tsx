f0d60054c95c31cfdc20e3307977aea511f89d82
MrDouglax <Meioameio8686@gmail.com>
2023-04-04 20:18:51 -0300

    fix

--- src/components/PossibleCouses/PossibleCourses.tsx
+++ src/components/PossibleCouses/PossibleCourses.tsx
@@ -5,7 +5,7 @@ import { allCampus } from '../../data/courses/allCampus';
 
 import { MdOutlineVerified } from "react-icons/md";
 
-export default function PossibleCourses({goal}:{goal: number}) {
+export default function PossibleCourses({ goal }: { goal: number }) {
 
   const [cotism, setCotism] = useState(true)
 
@@ -14,12 +14,16 @@ export default function PossibleCourses({goal}:{goal: number}) {
   return (
     <>
       <div className="inline cotism">
-        <p className="broad" onClick={() => {setCotism(false)}} style={{opacity: cotism ? '0.5': '1'}} >
-          Ampla concorrência
-        </p>
-        <p className="quotaholder" onClick={() => { setCotism(true) }} style={{ opacity: cotism ? '1' : '0.5' }}>
-          Cotista
-        </p>
+        <div className="broad" onClick={() => { setCotism(false) }} style={{ opacity: cotism ? '0.5' : '1' }}>
+          <p >
+            Ampla concorrência
+          </p>
+        </div>
+        <div className="quotaholder" onClick={() => { setCotism(true) }} style={{ opacity: cotism ? '1' : '0.5' }}>
+          <p>
+            Cotista
+          </p>
+        </div>
       </div>
       <div className="PossibleCourses">
         {/* Interação dos campus */}
@@ -31,9 +35,9 @@ export default function PossibleCourses({goal}:{goal: number}) {
               {campus.courses.map(course => {
                 return (
                   <div className='course inline'>
-                    <MdOutlineVerified className='icon' style={{ color: goal >= (cotism ? course.grades.quotaholder.lowest : course.grades.broadCompetition.lowest ) ? 'rgb(17, 255, 96)' : 'rgb(68, 68, 68)' }}/>
+                    <MdOutlineVerified className='icon' style={{ color: goal >= (cotism ? course.grades.quotaholder.lowest : course.grades.broadCompetition.lowest) ? 'rgb(17, 255, 96)' : 'rgb(68, 68, 68)' }} />
                     <div className='name' >{course.name} <div> {(cotism ? course.grades.quotaholder.lowest : course.grades.broadCompetition.lowest)} à {(cotism ? course.grades.quotaholder.highest : course.grades.broadCompetition.highest)}</div></div>
-                    
+
                   </div>
                 )
               })}
