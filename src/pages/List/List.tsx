03210e4f850697aa1a58992d61e85b190170fe12
MrDouglax <Meioameio8686@gmail.com>
2023-04-05 00:22:40 -0300

    fix

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -21,7 +21,7 @@ export default function List() {
         {periods.map((period, index) => {
           return (
             <Link to={period.period} key={index} className='Period' style={{ animation: `in ${1000 + index * 100}ms ease forwards`}}>
-              <h2>SSA entre 20{period.period.split('-')[0]} à 20{period.period.split('-')[1]}</h2>
+              <h2>SSA entre 20{period.period.split('-')[0]} e 20{period.period.split('-')[1]}</h2>
               <p>{period.desc}</p>
             </Link>
           )
