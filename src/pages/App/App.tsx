1158989b6eba975d9402b45bbf77339fcf32f7a4
MrDouglax <Meioameio8686@gmail.com>
2023-06-20 06:28:52 -0300

    file formatted

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,45 +1,44 @@
-import './App.scss'
-
-import { useParams } from 'react-router-dom'
+import { lazy } from "react";
+import { useParams } from "react-router-dom";
 import { MdWarningAmber } from "react-icons/md";
 
-import { allPeriods } from '../List/List'
-import Header from '../../components/Header/Header'
-import LastYear from './components/lastYear/lastYear'
-import OldYears from './components/oldYear/oldYear'
+import "./App.scss";
 
-import whatsapp from '/images/whatsapp.webp'
+const Header = lazy(() => import("../../components/Header/Header"));
+const FindGrade = lazy(() => import("./components/findGrade/findGrade"));
+
+import { allPeriods } from "../List/List";
+import whatsapp from "/images/whatsapp.webp";
 
 export default function App() {
+  const { period } = useParams();
 
-  const { period } = useParams()
-
-  const invalidPeriod = allPeriods.every((pd) => pd.period !== period)
+  const invalidPeriod = allPeriods.every((pd) => pd.period !== period);
   if (invalidPeriod) {
     return (
-      <div className='InvalidPeriod' >
-        <MdWarningAmber className='Icon' />
-        <p className='Text' >Algo parece errado... As datas não batem...</p>
+      <div className="InvalidPeriod">
+        <MdWarningAmber className="Icon" />
+        <p className="Text">Algo parece errado... As datas não batem...</p>
       </div>
-    )
+    );
   }
 
-  const lastYear = allPeriods.filter((pd) => pd.period === period)[0].lastYear
+  const lastYear = allPeriods.filter((pd) => pd.period === period)[0].lastYear;
 
   return (
     <div className="App">
-      <Header hasLink={true} backLink='/' showHelp={true} />
-      <div className='Content'>
-        <LastYear period={period as string} /> :
+      <Header hasLink={true} backLink="/" showHelp={true} />
+      <div className="Content">
+        <FindGrade period={period as string} />
       </div>
-      <p id='by'> By: Douglas
+      <p id="by">
+        {" "}
+        By: Douglas
         <a href="https://wa.me/5581996405552">
-          <img src={whatsapp} alt='whatsapp' />
+          <img src={whatsapp} alt="whatsapp" />
         </a>
       </p>
-      <div className='BottomDegrade'>
-
-      </div>
+      <div className="BottomDegrade"></div>
     </div>
-  )
+  );
 }
