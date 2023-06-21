9b4c8d226f7cc32f3f59070ba449dc57d8f51513
MrDouglax <Meioameio8686@gmail.com>
2023-06-21 19:50:07 -0300

    feat: indexed old and new pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import About from "./pages/About/About";
 // import GetCampusFromText from "./data/courses/scripts/getCampusFromText";
 
 const App = lazy(() => import("./pages/App/App"));
 const List = lazy(() => import("./pages/List/List"));
 const Help = lazy(() => import("./pages/Help/Help"));
 const HowUse = lazy(() => import("./pages/HowUse/HowUse"));
+const About = lazy(() => import("./pages/About/About"));
+const Novelty = lazy(() => import("./pages/Novelty/Novelty"));
+const NotFound = lazy(() => import("./pages/NotFound/NotFound"));
 
 const router = createBrowserRouter([
   {
@@ -48,6 +50,22 @@ const router = createBrowserRouter([
     element: (
       <Suspense fallback={<></>}>
         <About></About>
+      </Suspense>
+    ),
+  },
+  {
+    path: "/novelty",
+    element: (
+      <Suspense fallback={<></>}>
+        <Novelty></Novelty>
+      </Suspense>
+    ),
+  },
+  {
+    path: "*",
+    element: (
+      <Suspense fallback={<></>}>
+        <NotFound />
       </Suspense>
     ),
   },
