5ce7d000f7a7765e659e50289fdc02e300d2bdcf
MrDouglax <Meioameio8686@gmail.com>
2023-06-25 00:55:31 -0300

    fix

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
     <div className="NotFound">
       <MdReportGmailerrorred className="Icon" />
       <p className="Text">Página não encontrada...</p>
-      <a href="/" className="mainPage">
+      <a href="/home" className="mainPage">
         Voltar para a página principal
       </a>
     </div>
